6e95a200de63762c024bb4490a2de812fbc9ceea
Crystal <crystal.r.mcgowan@gmail.com>
2017-07-11 13:50:29 -0400

    Final

--- main.js
+++ main.js
@@ -15,40 +15,58 @@ function question1 () {
   //get total of all "price" keys
     sum += data[i].price
   }
-  console.log("The average price is"+ " " + Math.round(sum / data.length)
- + ".")
+  console.log("The average price is"+ " " + (sum / data.length).toFixed(2) + ".")
 }
-
-
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
 function question2 () {
-  
+  // array, items, cost, $14, $18
+  //Create an array. Find the prices of items. More than $14, less than $18
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].price >= 14 && data[i].price <= 18) {
+      console.log(data[i].title);
+    }
+  }
 }
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
 function question3 () {
-
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].currency_code === "GBP"){
+      console.log(data[i].title + data[i].price);
+    }
+  }
 }
 
 
 
 // 4: Display a list of all items who are made of wood.
 function question4 () {
-  // Answer:
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].materials = "wood"){
+      console.log(data[i].title);
+    }
+  }
 }
 
 
 // 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
 function question5 () {
-  // Answer:
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].materials.length <= 8) {
+      console.log(data[i].title + " " + data[i].materials.length + " " + data[i].materials);
+    }
+  }
 }
 
 
 // 6: How many items were made by their sellers?
-// Answer:
 function question6 () {
-  // Answer:
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].who_made === "i_did") {
+      console.log(data[i].who_made.length);
+    }
+  }
 }
